cc6a2f408d11d60d5948a7d69f82d99dbddaf534
a <a@enomacbook-pro.lan>
2023-11-18 12:15:44 +0900

    summarize information of volumeCredit

--- 1/print_invoices.js
+++ 1/print_invoices.js
@@ -10,6 +10,7 @@ export default function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -38,6 +39,13 @@ export default function statement(invoice, plays) {
         }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -50,14 +58,6 @@ function renderPlainText(data, plays) {
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
-
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat(
@@ -73,7 +73,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let aPerformance of data.performances) {
-            result += volumeCreditsFor(aPerformance);
+            result += aPerformance.volumeCredits;
         }
         return result;
     }
