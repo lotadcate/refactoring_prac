80f087b2a172c4517e136df67ede8fb4289547f4
a <a@enomacbook-pro.lan>
2023-11-18 13:38:26 +0900

    support HTML

--- 1/main.js
+++ 1/main.js
@@ -1,5 +1,6 @@
 import invoice from "./data/invoices.json" assert { type: "json" };
 import plays from "./data/plays.json" assert { type: "json" };
-import statement from "./print_invoices.js";
+import { statement, htmlStatement } from "./print_invoices.js";
 
 console.log(statement(invoice, plays));
+console.log(htmlStatement(invoice, plays));
