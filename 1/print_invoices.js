8b67a492607160ed707e9638c94b93003a99809c
a <a@enomacbook-pro.lan>
2023-11-18 09:31:58 +0900

    extract the part of price calculation

--- 1/print_invoices.js
+++ 1/print_invoices.js
@@ -16,25 +16,7 @@ export default function statement(invoice, plays) {
     for (let perf of invoice[0].performances) {
         // console.log(plays[perf["playID"]])
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
 
         volumeCredits += Math.max(perf.audience - 30, 0)
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
@@ -46,4 +28,26 @@ export default function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
 }
