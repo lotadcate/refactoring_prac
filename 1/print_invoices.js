3bd9df7030323fe24db8887b1c8cd9c6a0deac17
a <a@enomacbook-pro.lan>
2023-11-18 10:48:14 +0900

    extract the part of point calculation

--- 1/print_invoices.js
+++ 1/print_invoices.js
@@ -18,7 +18,7 @@ export default function statement(invoice, plays) {
     for (let perf of invoice[0].performances) {
         // console.log(plays[perf["playID"]])
 
-        volumeCredits += Math.max(perf.audience - 30, 0)
+        volumeCredits += volumeCreditsFor(perf);
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
@@ -52,7 +52,13 @@ function amountFor(aPerformance) {
     return result;
 }
 
-
 function playFor(aPerformance) {
     return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
 }
